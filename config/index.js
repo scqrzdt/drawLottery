06c5658c1897400434f1441a45a80c2bebca07bc
dick <503434074@qq.com>
2019-01-14 01:43:30 +0800

    dick

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/drawLottery/dist/', // old:'/'
+    assetsPublicPath: '/drawLottery/dist/', // old:'/'  /drawLottery/dist/
 
     /**
      * Source Maps
